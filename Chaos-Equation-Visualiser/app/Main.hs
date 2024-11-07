fc32304c3f197bbf452499376b330bdec011923a
Jamie Phillips <155266067+Jamiephillips915@users.noreply.github.com>
2024-11-07 11:26:32 +0000

    Update Main.hs

--- Chaos-Equation-Visualiser/app/Main.hs
+++ Chaos-Equation-Visualiser/app/Main.hs
@@ -9,16 +9,16 @@ yCoord :: GLfloat -> GLfloat -> [GLfloat]
 zCoord :: GLfloat -> GLfloat -> [GLfloat]
 pointGen :: GLfloat -> GLfloat -> [(GLfloat, GLfloat, GLfloat)]
 
-xCoord x r = iterate (\y -> r * y *(1 - y)) x
-yCoord i r = iterate (\y -> r * y *(1 - y)) i
-zCoord z r = iterate (\y -> r * y *(1 - y)) z
+xCoord x r = take 30 (iterate (\y -> r * y *(1 - y)) x)
+yCoord i r = take 30 (iterate (\y -> r * y *(1 - 0.6 * y)) i)
+zCoord z r = take 30 (iterate (\y -> r * y *(1 - 0.2 * y)) z)
     
 pointGen x r = [(xCoordinate, yCoordinate, zCoordinate) | (xCoordinate, yCoordinate, zCoordinate) <- zip3 (xCoord x r) (yCoord x r) (zCoord x r)]
 
 main = do
     _ <- getArgsAndInitialize        -- Initialize GLUT
     _ <- createWindow "Chaos Equation Visualiser"     -- Create the window
-    _ <- fullScreenToggle
+    --_ <- fullScreenToggle
     displayCallback $= display        -- Set display callback
     mainLoop                         -- Start the main loop
 
@@ -27,4 +27,6 @@ display :: DisplayCallback
 display = do
     clear [ColorBuffer]               -- Clear the color buffer
 
+    renderPrimitive Points $
+        mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) (pointGen 0.4 4)
     flush
